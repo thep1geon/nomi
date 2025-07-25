6b9acd380229f8b818f8db49c159d61fa2697505
thep1geon <hauptmaverick@gmail.com>
2025-07-24 23:10:06 -0400

    Add new compiler flag for verbose printing of compiler stages

--- src/cli.zig
+++ src/cli.zig
@@ -6,6 +6,7 @@ const Allocator = std.mem.Allocator;
 const ArgIterator = std.process.ArgIterator;
 
 const Error = error{
+    ExpectedArg,
     UnrecognizedOption,
     MissingInfile,
 };
@@ -17,12 +18,13 @@ const util = struct {
     var executable: []const u8 = "";
 
     fn usage() void {
+        // TODO: Fix where these are being printed to
         std.debug.print("Usage: {s} [options...] <infile>\n", .{executable});
         std.debug.print("Options:\n", .{});
         std.debug.print("   -o --output <outfile>   Specify the file path of the emitted object file\n", .{});
         std.debug.print("   -h --help               Display this usage menu and exit\n", .{});
+        std.debug.print("   -V <ast|ir>             Print the various stages of compilation. Use this flag multiple times to print multiple stages\n", .{});
         std.debug.print("   -v --version            Print the version and exit\n", .{});
-        return;
     }
 
     fn version() void {
@@ -37,6 +39,13 @@ pub const Options = struct {
     outfile: []const u8 = "output.o",
 
     early_exit: bool = false,
+
+    verbose: Verbose = .{},
+
+    const Verbose = struct {
+        ast: bool = false,
+        ir: bool = false,
+    };
 };
 
 pub fn parse_args() Error!Options {
@@ -57,6 +66,22 @@ pub fn parse_args() Error!Options {
         } else if (mem.eql(u8, arg, "-v") or mem.eql(u8, arg, "--version")) {
             util.version();
             break :blk;
+        } else if (mem.eql(u8, arg, "-V")) {
+            const next_arg = args.next() orelse {
+                util.usage();
+                std.debug.print("Expected verbose arg\n", .{});
+                return Error.ExpectedArg;
+            };
+
+            if (mem.eql(u8, next_arg, "ast")) {
+                opts.verbose.ast = true;
+            } else if (mem.eql(u8, next_arg, "ir")) {
+                opts.verbose.ir = true;
+            } else {
+                util.usage();
+                std.debug.print("Unrecognized verbose arg '{s}'\n", .{next_arg});
+                return Error.UnrecognizedOption;
+            }
         } else if (mem.eql(u8, arg, "-o") or mem.eql(u8, arg, "--output")) {
             // FIXME: Deal with actual error handling here.
             opts.outfile = args.next() orelse @panic("Expected outfile");
